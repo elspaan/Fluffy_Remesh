2ddc1d9c49100d2857e5e5c429487cf9e0749326
Elouan Spaan <spaan.elouan@gmail.com>
2026-02-19 19:41:15 +0100

    MOre tutorial 1

--- test.cpp
+++ test.cpp
@@ -10,38 +10,59 @@ run code: ./name-of-program
 
 // define CGAL kernel with double type coordinates and create an alias (Kernel)
 typedef CGAL::Simple_cartesian<double> Kernel;  // Cartesian grid
-typedef Kernel::Point_2 Point_2; // This is a type representing points in 2D with the alias Point_2
-typedef Kernel::Segment_2 Segment_2; // THis is a type representing segments in 2D with the alias Segment_2
-
+typedef Kernel::Point_2 Point_2; // This is a type representing points in 2D with the alias Point_2; the suffix ( _X) is the dimension
+typedef Kernel::Segment_2 Segment_2; // This is a type representing segments in 2D with the alias Segment_2
+ 
 
 int main() {
     /* Create some points and segments in 2D space and compute
         - distance btwn points
         - distance btwn point and segment
-        - 
-    
+        - relative orientations between the points w.r.t. the coordinate axes
     */
-    // create 2D points and print its
-    Point_2 p(1.0, 1.0), q(10.0, 10.0);  // coordinates (x, y)
+
+    // 1) point-point
+    // create 2D points
+    Point_2 p(1.0, 1.0), q(10.0, 10.0);  // coordinates (x, y) with x right and y up
 
     // print point to the terminal. Note: std::endl = << "\n", where the latter is actually slightly faster
-    std::cout << "p = " << p << std::endl;
-    std::cout << "q = " << q << std::endl;
+    std::cout << "p: " << p << std::endl;
+    std::cout << "q: " << q << std::endl;
     std::cout << "q is also " << q.x() << " " << q.y() << std::endl;
     
     // "Shortest euclidian distance between two geometric object"
-    // = the shortest, straightest distance
-    std::cout << "Squared-distance(p,q) = " << CGAL::squared_distance(p, q) << std::endl;
+    // = the shortest, straightest distance _squared_ (so not the act. distance)
+    // direction invariant - switching object positions does not matter
+    std::cout << "Squared-distance(p,q): " << CGAL::squared_distance(q, p) << std::endl;
     
-    // segment time - create a segment of points p and q, and compute the distance between
+    // 2) point-segment
+    // create a segment of points p and q, and compute the distance between
     // it and a 3rd point m.
     Segment_2 s(p,q);
     Point_2 m(5, 9);
 
-    std::cout << "m = " << m << std::endl;
-    std::cout << "distance segment to m = " << CGAL::squared_distance(s,m) << std::endl;
+    std::cout << "m: " << m << std::endl;
+    std::cout << "distance segment to m: " << CGAL::squared_distance(s,m) << std::endl;
     
+    // 3) Orientations
+    // Check how the points are positioned relative to each other (collinear, left hook or right hook)
+    // and print the result in text
+    // switch = a kind of if-statement but for many code blocks. Usually more efficient than nested if/else
+    // case = a specific instance. So if case 1, then: execute indented code; break;
+    std::cout << "p, q, and m ";
+    switch (CGAL::orientation(p,q,m)){
+    case CGAL::COLLINEAR:
+        std::cout << "are collinear\n";
+        break;
+    case CGAL::LEFT_TURN:
+        std::cout << "make a left turn\n";
+        break;
+    case CGAL::RIGHT_TURN:
+        std::cout << "make a right turn\n";
+        break;
+    }    
+    
+    std::cout << "midpoint (p,q): " << CGAL::midpoint(p,q) << std::endl;
 
-    
     return 0; // if successful, return 0
 }
